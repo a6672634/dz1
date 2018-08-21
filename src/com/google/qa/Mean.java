d045e35d3906deb2ba22fd5f4ceb1b54639b2e9d
qa <xbx@jn.dd>
2018-08-21 20:56:43 +0300

    Initial commit

---
+++ src/com/google/qa/Mean.java
@@ -0,0 +1,55 @@
+package com.google.qa;
+
+public class Mean {
+    public static void main(String[] args) {
+
+        //* дано 1865 часов. Вывести на экран колличество целых дней и часов в виде : "Было дано 1865 часов.Это равно х дней и У часов.
+       float chas = 1865;
+       float dney;
+       float chasov = 0;
+
+       dney = chas/24;
+       chasov%= dney;
+
+        System.out.println(dney + " "+chasov);
+
+
+
+
+        /*
+        float rad = 125.12f;  Алгоритм вычиления площади круга
+        float pi = 3.14f;
+        float s ;
+
+        s= (pi *(rad*rad));
+
+        System.out.println(s);
+
+
+        /*
+        int a = 150; реализовать алгоритм вычисляющий среднеарифметическое 4х чмсел и выводящий это на экран
+        int b = 317;
+        int c = 535;
+        int d = 248;
+        float x;
+         x = (a+b+c+d)/4;
+        System.out.println(x);
+        /*float b = -1.598f;
+        int c = 12;
+        a%= c;*/
+        /*
+        a = -a; унарный оператор . присваивает значение самому числу.
+        b = +b;   */
+
+       /*
+       ++a;
+       ++b;
+       b=++b;
+       присваивает еденицу. применяеться для счетчиков.  */
+
+       /* System.out.println("число а = " + a);*/
+        /* System.out.println("число b = " + b);
+        System.out.println("число с =" + c); */
+
+    }
+}
